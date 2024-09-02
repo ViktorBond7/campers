873a598cf1b079cd8a7b363332e4d2416b47962c
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-09-02 11:32:12 +0200

    reviews

--- src/components/VehicleDetails/VehicleDetails.jsx
+++ src/components/VehicleDetails/VehicleDetails.jsx
@@ -5,32 +5,37 @@ const VehicleDetails = ({ camper }) => {
     <div>
       <h2 className={css.page}>Vehicle details</h2>
       <table className={css.containerTable}>
-        <tr>
-          <th className={css.leftAlign}>Form</th>
-          <th className={css.rightAlign}>Panel truck</th>
-        </tr>
-        <tr>
-          <td className={css.leftAlign}>Length</td>
-          <td className={css.rightAlign}>{camper.length}</td>
-        </tr>
-        <tr>
-          <td className={css.leftAlign}>Width</td>
-          <td className={css.rightAlign}>{camper.width}</td>
-        </tr>
-        <tr>
-          <td className={css.leftAlign}>Height</td>
-          <td className={css.rightAlign}>{camper.height}</td>
-        </tr>
-        <tr>
-          <td className={css.leftAlign}>Tank</td>
-          <td className={css.rightAlign}>{camper.tank}</td>
-        </tr>
-        <tr>
-          <td className={css.leftAlign}>Consumption</td>
-          <td className={css.rightAlign}>{camper.consumption}</td>
-        </tr>
+        <thead>
+          <tr>
+            <th className={css.leftAlign}>Form</th>
+            <th className={css.rightAlign}>Panel truck</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={css.leftAlign}>Length</td>
+            <td className={css.rightAlign}>{camper.length}</td>
+          </tr>
+          <tr>
+            <td className={css.leftAlign}>Width</td>
+            <td className={css.rightAlign}>{camper.width}</td>
+          </tr>
+          <tr>
+            <td className={css.leftAlign}>Height</td>
+            <td className={css.rightAlign}>{camper.height}</td>
+          </tr>
+          <tr>
+            <td className={css.leftAlign}>Tank</td>
+            <td className={css.rightAlign}>{camper.tank}</td>
+          </tr>
+          <tr>
+            <td className={css.leftAlign}>Consumption</td>
+            <td className={css.rightAlign}>{camper.consumption}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
 };
+
 export default VehicleDetails;
